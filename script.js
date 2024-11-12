32d906217e7c9b00b5dd70e6d1163b7a38db4ff9
abdulaitobi <abdulaitobi14@gmail.com>
2024-11-11 22:39:38 -0800

    Finished product

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ var winningValues = [
     [2,5,8],
     [2,4,6],
     [3,4,5],
-    [6,7,8]
+    [6,7,8] //  winning sequences
 ];
+
 // function for player turns
 function turnCounter(val) {
     return val === 0 ? 1 : 0;
 }
 
+//  console gameboard
 const gameBoard = {
     array: Array(9).fill(null)
 };
@@ -28,39 +30,68 @@ const playerTwo = {
     positions: []
 };
 
-function game(){
-    for(let i = 0; i < winningValues.length; i++){
-        if(winningValues[i].every(element => playerOne.positions.includes(element))) {
-            console.log("You win");
+//  function to check if a player has won
+function checkWin(player) {
+    return winningValues.some(combination => 
+        combination.every(index => player.positions.includes(index))
+    );
+}
+
+//  function to disable cells after game is finished
+function disableCells() {
+    const elements = document.querySelectorAll('.cell');
+    elements.forEach(element => {
+        element.removeEventListener('click',handleClick);
+    });
+}
+
+function handleClick(event) {
+    document.getElementById('refreshBtn').addEventListener('click', function(){
+        location.reload();
+    });
+    let cell = parseInt(event.target.id, 10); // Convert cell ID to number
+    if (gameBoard.array[cell] !== null) {
+        alert('Cell taken');
+        return;
+    }
+
+    if (counter === 0) {
+        event.target.innerHTML += '<i class="fa-solid fa-x fa-7x"></i>';
+        gameBoard.array[cell] = playerOne.marker;
+        playerOne.positions.push(cell);
+        event.target.removeEventListener('click', handleClick); // Disable click listener for this cell
+        if (checkWin(playerOne)) {
+            document.getElementById('winner').innerHTML = "Player One Wins!";
+            disableCells(); // Disable all further moves after player one wins
             return;
         }
-        else if(winningValues[i].every(element => playerTwo.positions.includes(element))){
-            console.log("You lose");
-            return;
-        }
-        else if(gameBoard.array.every(element => element !== null && element !== undefined)){
-            console.log("Its a draw");
+    } else {
+        event.target.innerHTML += '<i class="fa-solid fa-o fa-7x"></i>';
+        gameBoard.array[cell] = playerTwo.marker;
+        playerTwo.positions.push(cell);
+        event.target.removeEventListener('click', handleClick); // Disable click listener for this cell
+        if (checkWin(playerTwo)) {
+            document.getElementById('winner').innerHTML = "Player Two Wins!";
+            disableCells(); // Disable all further moves after player two wins
             return;
         }
     }
-    let cell = parseInt(prompt("Enter the cell number (0-8): Player" + counter+1));
-    if(gameBoard.array[cell] !== null){
-        console.log('cell taken');
-        game();
+
+    // Check for a draw
+    if (gameBoard.array.every(cell => cell !== null)) {
+        document.getElementById('winner').innerHTML = "It's a draw!";
+        disableCells(); // Disable further moves in case of a draw
+        return;
     }
-    else{
-        if(counter === 0){
-            gameBoard.array[cell] = playerOne.marker;
-            playerOne.positions.push(cell);
-        } 
-        else{
-            gameBoard.array[cell] = playerTwo.marker;
-            playerTwo.positions.push(cell);
-        }
-        counter = turnCounter(counter);
-    }
-    console.log("Game Board:", gameBoard.array);
-    game();
+
+    counter = turnCounter(counter);
+}
+
+function game() {
+    const elements = document.querySelectorAll('.cell');
+    elements.forEach((element) => {
+        element.addEventListener('click', handleClick);
+    });
 }
 
 game();
